113ddef5166fd28eba23248a5940facc15390920
Dorota Jarecka <djarecka@gmail.com>
2025-01-05 23:31:08 +0000

    week2

--- week2/prime_factors.py
+++ week2/prime_factors.py
@@ -8,6 +8,10 @@ def prime_factors(n):
     Returns:
         list: A list of prime factors of n.
     """
+    # checking if the number is an integer
+    if not isinstance(n, int):
+        raise ValueError(f"The number has to be integer, but {n} provided")
+
     factors = []
     # Handle the factor of 2
     while n % 2 == 0:
@@ -19,13 +23,8 @@ def prime_factors(n):
             factors.append(i)
             n //= i
 
+    # If n is still greater than 2, it must be prime
+    if n > 2:
+        factors.append(n)
+
     return factors
-
-def test_prime_factors():
-    assert prime_factors(8) == [2, 2, 2], "The prime factors of 8 are [2, 2, 2]"
-    assert prime_factors(18) == [2, 3, 3], "The prime factors of 18 are [2, 3, 3]"
-    assert prime_factors(108) == [2, 2, 3, 3, 3], "The prime factors of 108 are [2, 2, 3, 3, 3]"
-
-if __name__ == "__main__":
-    test_prime_factors()
-    print("Everything passed")
