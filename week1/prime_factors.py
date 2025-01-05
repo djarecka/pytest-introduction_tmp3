2674b776901d8def8a0200fa12ac3a6ae2da90de
Dorota Jarecka <djarecka@gmail.com>
2025-01-05 23:23:32 +0000

    test

--- week1/prime_factors.py
+++ week1/prime_factors.py
@@ -14,7 +14,7 @@ def prime_factors(n):
         factors.append(2)
         n //= 2
     # Handle odd factors from 3 upwards
-    for i in range(3, int(n ** 0.5), 2):
+    for i in range(3, int(n ** 0.5) + 1, 2):
         while n % i == 0:
             factors.append(i)
             n //= i
@@ -22,8 +22,10 @@ def prime_factors(n):
     return factors
 
 def test_prime_factors():
-    assert prime_factors(8) == [2, 2, 2]
-
+    assert prime_factors(8) == [2, 2, 2], "The prime factors of 8 are [2, 2, 2]"
+    assert prime_factors(18) == [2, 3, 3], "The prime factors of 18 are [2, 3, 3]"
+    assert prime_factors(108) == [2, 2, 3, 3, 3], "The prime factors of 108 are [2, 2, 3, 3, 3]"
 
 if __name__ == "__main__":
     test_prime_factors()
+    print("Everything passed")
